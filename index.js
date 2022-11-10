9f0e5d627f5d0e88db3fb5c097112e02e2f9be3a
kashishdipro <kamanashishdatta@gmail.com>
2022-11-10 15:27:25 +0600

    fetcing items from db

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kdtr5cm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const itemCollection = client.db("ktKitchen").collection("foodItems");
+        app.get('/items', async(req, res) =>{
+            const query = {}
+            const cursor = itemCollection.find(query);
+            const cursorLimit = itemCollection.find(query).limit(3);
+            const limiteditems = await cursorLimit.toArray();
+            const items = await cursor.toArray();
+            res.send({limiteditems, items});
+        })
+    }finally{
+
+    }
+}
+run().catch(error => console.error(error))
 
 
 app.get('/', (req, res) =>{
